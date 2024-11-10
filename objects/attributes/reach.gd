9f1a29cb52231bc919e57b2dfeeaa339bd8ff820
Ctharo <88241793+Ctharo@users.noreply.github.com>
2024-11-09 21:41:40 -0800

    Update reach.gd

--- objects/attributes/reach.gd
+++ objects/attributes/reach.gd
@@ -23,6 +23,7 @@ func _init_properties() -> void:
 		Property.create("food_in_range")
 			.of_type(Property.Type.FLOAT)
 			.with_attribute(name)
+			.with_dependencies(["range"])
 			.with_getter(Callable(self, "_get_food_in_range"))
 			.described_as("Food within reach range")
 			.build()
