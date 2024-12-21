a8906eb563bdc9cce06a01ee511e34e36d94a431
Ctharo <88241793+Ctharo@users.noreply.github.com>
2024-12-21 06:04:30 -0800

    Update debug_logger.gd

--- logging/debug_logger.gd
+++ logging/debug_logger.gd
@@ -32,7 +32,7 @@ enum Category {
 
 #region Configuration
 ## Current log level
-static var log_level := LogLevel.TRACE
+static var log_level := LogLevel.DEBUG
 
 ## Show context in logs
 static var show_context: bool = false
