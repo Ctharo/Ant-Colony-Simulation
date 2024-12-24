579cce8d64ab5eb54c9b760a7e7edc34f7f286e1
Ctharo <88241793+Ctharo@users.noreply.github.com>
2024-12-24 11:55:58 -0800

    Update context_menu_manager.gd
    Adjusting spawn count

--- ui/sandbox/context_menu_manager.gd
+++ ui/sandbox/context_menu_manager.gd
@@ -57,7 +57,7 @@ func show_empty_context_menu(world_pos: Vector2) -> void:
 func _on_spawn_colony_requested(position: Vector2) -> void:
 	var colony = colony_manager.spawn_colony_at(camera.ui_to_global(position))
 	world.add_child(colony)
-	colony.spawn_ants(10)
+	colony.spawn_ants(5)
 
 func clear_active_menu() -> void:
 	if is_instance_valid(active_context_menu):
@@ -67,7 +67,7 @@ func clear_active_menu() -> void:
 #region Colony Handlers
 func _on_colony_spawn_ants_requested(colony: Colony) -> void:
 	if is_instance_valid(colony):
-		colony.spawn_ants(10, true)
+		colony.spawn_ants(5)
 
 func _on_colony_info_requested(colony: Colony) -> void:
 	if is_instance_valid(colony):
