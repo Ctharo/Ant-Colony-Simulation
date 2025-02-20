f68bf4cbb504fb248b47e5c55a82fcee51b74a61
Ctharo <88241793+Ctharo@users.noreply.github.com>
2025-02-20 08:17:31 -0800

    Revert "Functional changes (broken)"
    This reverts commit 4f18668f68c5a05b884792c2f66a1064095d5bff.

--- autoloads/heatmap_manager.gd
+++ autoloads/heatmap_manager.gd
@@ -8,16 +8,18 @@ const STYLE = {
 }
 
 #region Member Variables
-var _heatmaps: Dictionary = {}  # name -> {config: Pheromone, chunks: Dictionary}
-var _entities: Dictionary = {}  # entity_id -> {type: String, colony_id: int}
-var _debug_settings: Dictionary = {}  # entity_id -> bool
+var map_size: Vector2
 var update_thread: Thread
 var update_lock: Mutex
+var _nav_map: RID
 var camera: Camera2D
+var _heatmaps: Dictionary[String, HeatmapInstance] = {} 
+var _debug_settings: Dictionary = {}
+var update_timer: float = 0.0
+var update_interval: float = 1.0
 var logger: Logger
 var _is_quitting: bool = false
 var _last_decay_time: int = 0
-var update_interval: float = 1.0
 #endregion
 
 #region Inner Classes
@@ -29,23 +31,23 @@ class HeatCell:
 		if not sources.has(entity_id):
 			sources[entity_id] = 0.0
 		sources[entity_id] = minf(sources[entity_id] + amount, max_heat)
-		heat = HeatmapUtils.calculate_total_heat(sources.values())
+		heat = HeatmapUtils.HeatCalculator.calculate_total_heat(sources.values())
 
 	func remove_source(entity_id: int) -> void:
 		sources.erase(entity_id)
-		heat = HeatmapUtils.calculate_total_heat(sources.values())
+		heat = HeatmapUtils.HeatCalculator.calculate_total_heat(sources.values())
 
 	func decay(delta: float, decay_rate: float) -> bool:
 		var any_active = false
 		for entity_id in sources:
-			sources[entity_id] = HeatmapUtils.apply_decay(
+			sources[entity_id] = HeatmapUtils.HeatCalculator.apply_decay(
 				sources[entity_id],
 				decay_rate,
 				delta
 			)
 			if sources[entity_id] > 0:
 				any_active = true
-		heat = HeatmapUtils.calculate_total_heat(sources.values())
+		heat = HeatmapUtils.HeatCalculator.calculate_total_heat(sources.values())
 		return any_active
 
 	func get_total_heat_for_colony(colony_id: int) -> float:
@@ -56,7 +58,7 @@ class HeatCell:
 				var source_colony_id: int = source.colony.get_instance_id() if source is Ant else source.get_instance_id()
 				if source_colony_id == colony_id:
 					colony_sources.append(sources[source_id])
-		return HeatmapUtils.calculate_total_heat(colony_sources)
+		return HeatmapUtils.HeatCalculator.calculate_total_heat(colony_sources)
 
 class HeatChunk:
 	var cells: Dictionary[Vector2i, HeatCell] = {}  # Vector2i -> HeatCell
@@ -84,7 +86,7 @@ class HeatChunk:
 			cells.erase(pos)
 
 		last_update_time = Time.get_ticks_msec()
-		return true ## What should this return?
+		return HeatmapUtils.StateUtils.is_chunk_active(cell_heats)
 
 class HeatmapInstance:
 	var chunks: Dictionary[Vector2i, HeatChunk] = {}  # Vector2i -> HeatChunk
@@ -99,8 +101,6 @@ class HeatmapInstance:
 		return chunks[chunk_pos]
 #endregion
 
-
-
 func _init() -> void:
 	logger = Logger.new("heatmap_manager", DebugLogger.Category.MOVEMENT)
 	update_lock = Mutex.new()
@@ -109,130 +109,14 @@ func _init() -> void:
 
 func _ready() -> void:
 	_start_update_thread()
-	
-## Create a new heatmap type from pheromone configuration
+
+## Generate [HeatmapManager.HeatmapInstance] that represents the [Pheromone].
 func create_heatmap_type(pheromone: Pheromone) -> void:
-	var heatmap_name := pheromone.name.to_lower()
+	var heatmap_name = pheromone.name.to_lower()
 	if not _heatmaps.has(heatmap_name):
-		_heatmaps[heatmap_name] = {
-			"config": pheromone,
-			"chunks": {}  # chunk_pos -> {cells: Dictionary, last_update: int}
-		}
-
-#region Entity Management
-## Register entity with the heatmap system
-func register_entity(entity: Node2D) -> void:
-	var entity_id := entity.get_instance_id()
-	if not _entities.has(entity_id):
-		_entities[entity_id] = {
-			"type": "ant" if entity is Ant else "colony",
-			"colony_id": entity.colony.get_instance_id() if entity is Ant else entity_id
-		}
-		_debug_settings[entity_id] = false
-		logger.debug("Registered entity %s" % entity.name)
-
-## Remove entity from the heatmap system
-func unregister_entity(entity: Node2D) -> void:
-	var entity_id := entity.get_instance_id()
-	
-	update_lock.lock()
-	_entities.erase(entity_id)
-	_debug_settings.erase(entity_id)
-	
-	# Remove entity from all heatmaps
-	for heatmap in _heatmaps.values():
-		for chunk in heatmap.chunks.values():
-			for cell in chunk.cells.values():
-				cell.sources = cell.sources.filter(
-					func(id: int) -> bool: return id != entity_id
-				)
-	update_lock.unlock()
-	
-	logger.debug("Unregistered entity %s" % entity.name)
-
-func debug_draw(entity: Node2D, enabled: bool) -> void:
-	_debug_settings[entity.get_instance_id()] = enabled
-	queue_redraw()
-#endregion
-
-#region Heat Management
-## Update heat for an entity
-func update_entity_heat(
-	entity: Node2D,
-	delta: float,
-	heat_type: String,
-	factor: float = 1.0
-) -> void:
-	if not _heatmaps.has(heat_type):
-		return
-
-	var entity_id := entity.get_instance_id()
-	var world_pos := entity.global_position
-	var world_cell := HeatmapUtils.world_to_grid(world_pos, STYLE.CELL_SIZE)
-	var base_heat: float = _heatmaps[heat_type].config.generating_rate * delta * factor
-
-	update_lock.lock()
-	_update_cell_heat(entity_id, world_cell, base_heat, heat_type)
-	update_lock.unlock()
-
-## Update heat for cells around entity
-func _update_cell_heat(
-	entity_id: int,
-	center_cell: Vector2i,
-	base_heat: float,
-	heat_type: String
-) -> void:
-	var heatmap: HeatmapInstance = _heatmaps[heat_type]
-	var cells := HeatmapUtils.get_cells_in_radius(
-		center_cell,
-		heatmap.config.heat_radius
-	)
-
-	for cell in cells:
-		var distance := center_cell.distance_to(cell)
-		var heat := HeatmapUtils.calculate_cell_heat(
-			base_heat,
-			distance,
-			STYLE.MAX_HEAT
+		_heatmaps[heatmap_name] = HeatmapInstance.new(
+			pheromone
 		)
-		_add_heat_to_cell(entity_id, cell, heat, heat_type)
-
-## Add heat to a specific cell
-func _add_heat_to_cell(
-	entity_id: int,
-	world_cell: Vector2i,
-	amount: float,
-	heat_type: String
-) -> void:
-	var coords := HeatmapUtils.world_to_chunk_coords(world_cell, STYLE.CHUNK_SIZE)
-	var chunk_pos: Vector2i = coords.chunk
-	var local_pos: Vector2i = coords.local
-	
-	# Ensure chunk exists
-	if not _heatmaps[heat_type].chunks.has(chunk_pos):
-		_heatmaps[heat_type].chunks[chunk_pos] = {
-			"cells": {},
-			"last_update": Time.get_ticks_msec()
-		}
-	
-	var chunk: Dictionary = _heatmaps[heat_type].chunks[chunk_pos]
-	
-	# Ensure cell exists
-	if not chunk.cells.has(local_pos):
-		chunk.cells[local_pos] = {
-			"sources": {},
-			"heat": 0.0
-		}
-	
-	var cell: Dictionary = chunk.cells[local_pos]
-	cell.sources = HeatmapUtils.add_heat_source(
-		cell.sources,
-		entity_id,
-		amount,
-		STYLE.MAX_HEAT
-	)
-	cell.heat = HeatmapUtils.calculate_total_heat(cell.sources.values())
-#endregion
 
 #region Thread Management
 func _start_update_thread() -> void:
@@ -241,49 +125,33 @@ func _start_update_thread() -> void:
 
 func _update_heatmap_thread() -> void:
 	while not _is_quitting:
-		var current_time := Time.get_ticks_msec()
-		var time_since_decay := (current_time - _last_decay_time) / 1000.0
+		var current_time: int = Time.get_ticks_msec()
+		var time_since_decay: float = (current_time - _last_decay_time) / 1000.0
 
+		# Only attempt update if enough time has passed
 		if time_since_decay >= update_interval:
+			# Try to acquire lock, don't block if can't get it
 			if update_lock.try_lock():
 				_process_decay(time_since_decay)
 				update_lock.unlock()
 				_last_decay_time = current_time
 
 		call_thread_safe("queue_redraw")
-		OS.delay_msec(int(update_interval * 100))
+		# Always sleep to prevent tight loop
+		OS.delay_msec(int(update_interval * 100))  # Check 10 times per interval
 
 func _process_decay(delta: float) -> void:
-	for heatmap_data in _heatmaps.values():
-		var chunks_to_remove := []
-		
-		for chunk_pos in heatmap_data.chunks:
-			var chunk: Dictionary = heatmap_data.chunks[chunk_pos]
-			var cells_to_remove := []
-			
-			for local_pos in chunk.cells:
-				var cell : Dictionary = chunk.cells[local_pos]
-				cell.sources = HeatmapUtils.update_heat_sources(
-					cell.sources,
-					heatmap_data.config.decay_rate,
-					delta
-				)
-				
-				if cell.sources.is_empty():
-					cells_to_remove.append(local_pos)
-				else:
-					cell.heat = HeatmapUtils.calculate_total_heat(
-						cell.sources.values()
-					)
-			
-			for pos in cells_to_remove:
-				chunk.cells.erase(pos)
-				
-			if chunk.cells.is_empty():
+	for heat_type in _heatmaps:
+		var heatmap: HeatmapInstance = _heatmaps[heat_type]
+		var chunks_to_remove: Array = []
+
+		for chunk_pos in heatmap.chunks:
+			var chunk: HeatChunk = heatmap.chunks[chunk_pos]
+			if not chunk.update(delta, heatmap.config.decay_rate):
 				chunks_to_remove.append(chunk_pos)
-		
-		for pos in chunks_to_remove:
-			heatmap_data.chunks.erase(pos)
+
+		for chunk_pos in chunks_to_remove:
+			heatmap.chunks.erase(chunk_pos)
 
 func _cleanup_thread() -> void:
 	if update_thread and update_thread.is_started():
@@ -295,86 +163,226 @@ func _notification(what: int) -> void:
 		_cleanup_thread()
 #endregion
 
-#region Heat Queries
-## Get heat value at entity position
+#region Setup Functions
+func setup_navigation() -> void:
+	_nav_map = get_world_2d().get_navigation_map()
+
+func setup_camera(p_camera: Camera2D) -> void:
+	camera = p_camera
+#endregion
+
+#region Entity Management
+## Register entity with the heatmap system
+func register_entity(entity: Node2D) -> void:
+	var entity_id: int = entity.get_instance_id()
+	if not _debug_settings.has(entity_id):
+		_debug_settings[entity_id] = false
+		logger.debug("Registered entity %s" % entity.name)
+
+func unregister_entity(entity: Node2D) -> void:
+	var entity_id: int = entity.get_instance_id()
+	# Prepare data outside lock
+	var entity_data = {
+		"id": entity_id,
+		"name": entity.name
+	}
+
+	update_lock.lock()
+	# Quick removal operation
+	_debug_settings.erase(entity_id)
+	for heatmap in _heatmaps.values():
+		for chunk in heatmap.chunks.values():
+			for cell in chunk.cells.values():
+				cell.remove_source(entity_id)
+	update_lock.unlock()
+
+	logger.debug("Unregistered entity %s" % entity_data.name)
+
+func debug_draw(entity: Node2D, enabled: bool) -> void:
+	_debug_settings[entity.get_instance_id()] = enabled
+	queue_redraw()
+#endregion
+
+#region Heat Management
+## Adds heat from an entity to a cell
+func update_entity_heat(entity: Node2D, delta: float, heat_type: String, factor: float = 1.0) -> void:
+	if not _heatmaps.has(heat_type):
+		return
+
+	# Prepare data outside lock
+	var update_data = {
+		"entity_id": entity.get_instance_id(),
+		"center_cell": NavigationUtils.GridUtils.world_to_cell(entity.global_position, STYLE["CELL_SIZE"]),
+		"base_heat": _heatmaps[heat_type].config.generating_rate * delta * factor,
+		"heat_type": heat_type
+	}
+
+	update_lock.lock()
+	_update_movement_heat(update_data)
+	update_lock.unlock()
+
+func _update_movement_heat(data: Dictionary) -> void:
+	var heatmap := _heatmaps[data.heat_type]
+	var cells_to_update := HeatmapUtils.StateUtils.get_cells_to_update(
+		data.center_cell,
+		heatmap.config.heat_radius
+	)
+
+	for cell in cells_to_update:
+		var distance: float = data.center_cell.distance_to(cell)
+		var heat := HeatmapUtils.HeatCalculator.calculate_cell_heat(
+			data.base_heat,
+			distance,
+			STYLE.MAX_HEAT
+		)
+		_add_heat_to_cell(data.entity_id, cell, heat, data.heat_type)
+		
+func _add_heat_to_cell(entity_id: int, world_cell: Vector2i, amount: float, heat_type: String) -> void:
+	var chunk_pos := NavigationUtils.GridUtils.world_to_chunk(world_cell, STYLE.CHUNK_SIZE)
+	var local_pos := NavigationUtils.GridUtils.world_to_local_cell(world_cell, STYLE.CHUNK_SIZE)
+	var heatmap := _heatmaps[heat_type]
+	var chunk := heatmap.get_or_create_chunk(chunk_pos)
+	var cell := chunk.get_or_create_cell(local_pos)
+	cell.add_heat(entity_id, amount, STYLE.MAX_HEAT)
+
+#region Heat Direction Calculation
+
+func _calculate_cell_influence(cell_data: Dictionary, query_data: Dictionary) -> float:
+	var cell: HeatCell = cell_data.cell
+	var total_heat: float = 0.0
+
+	for source_id in cell.sources:
+		if source_id == query_data.entity_id:  # Don't be influenced by own heat
+			continue
+		total_heat += cell.sources[source_id]
+
+	return total_heat
+
 func get_heat_at_position(entity: Node2D, heat_type: String) -> float:
 	if not _heatmaps.has(heat_type):
 		return 0.0
 
-	var entity_data: Dictionary = _entities[entity.get_instance_id()]
-	var world_cell := HeatmapUtils.world_to_grid(
-		entity.global_position,
-		STYLE.CELL_SIZE
-	)
+	var query_data = {
+		"colony_id": entity.colony.get_instance_id() if entity is Ant else entity.get_instance_id(),
+		"world_cell": NavigationUtils.GridUtils.world_to_cell(entity.global_position, STYLE.CELL_SIZE),
+		"heat_type": heat_type
+	}
 
-	var result := 0.0
+	var result: float = 0.0
 	update_lock.lock()
-	result = _get_cell_heat(world_cell, heat_type, entity_data.colony_id)
+	result = _get_heat_for_query(query_data)
 	update_lock.unlock()
 
 	return result
 
-## Get heat value for a specific cell
-func _get_cell_heat(
-	world_cell: Vector2i,
-	heat_type: String,
-	colony_id: int
-) -> float:
-	var coords := HeatmapUtils.world_to_chunk_coords(world_cell, STYLE.CHUNK_SIZE)
-	var chunk_pos:Vector2i= coords.chunk
-	var local_pos :Vector2i= coords.local
-	
-	if not _heatmaps[heat_type].chunks.has(chunk_pos):
+func _get_heat_for_query(data: Dictionary) -> float:
+	var chunk_pos: Vector2i = NavigationUtils.GridUtils.world_to_chunk(data.world_cell, STYLE.CHUNK_SIZE)
+	var local_pos: Vector2i = NavigationUtils.GridUtils.world_to_local_cell(data.world_cell, STYLE.CHUNK_SIZE)
+	var heatmap: HeatmapInstance = _heatmaps[data.heat_type]
+
+	if not heatmap.chunks.has(chunk_pos):
 		return 0.0
-		
-	var chunk :Dictionary= _heatmaps[heat_type].chunks[chunk_pos]
+
+	var chunk: HeatChunk = heatmap.chunks[chunk_pos]
 	if not chunk.cells.has(local_pos):
 		return 0.0
-		
-	var cell :Dictionary= chunk.cells[local_pos]
-	return HeatmapUtils.get_colony_heat(cell.sources, _entities, colony_id)
+
+	var cell: HeatCell = chunk.cells[local_pos]
+	return cell.get_total_heat_for_colony(data.colony_id)
 #endregion
 
-#region Drawing
+#region Drawing and Visualization
 func _draw() -> void:
 	if not camera:
 		return
 
 	update_lock.lock()
 	for heat_type in _heatmaps:
-		_draw_heatmap(_heatmaps[heat_type])
+		var heatmap: HeatmapInstance = _heatmaps[heat_type]
+		_draw_heatmap(heatmap)
 	update_lock.unlock()
 
-func _draw_heatmap(heatmap_data: Dictionary) -> void:
-	for chunk_pos in heatmap_data.chunks:
-		var chunk: HeatChunk = heatmap_data.chunks[chunk_pos]
+func _draw_heatmap(heatmap: HeatmapInstance) -> void:
+	for chunk_pos in heatmap.chunks:
+		var chunk: HeatChunk = heatmap.chunks[chunk_pos]
 		for local_pos in chunk.cells:
-			var cell := chunk.cells[local_pos]
-			var world_cell := HeatmapUtils.chunk_to_world_coords(
-				chunk_pos,
-				local_pos,
-				STYLE.CHUNK_SIZE
-			)
-			
-			var visible_heat := HeatmapUtils.filter_visible_sources(
-				cell.sources,
-				_debug_settings,
-				_entities
-			)
-			
+			var cell: HeatCell = chunk.cells[local_pos]
+			var world_cell: Vector2i = NavigationUtils.GridUtils.chunk_to_world_cell(chunk_pos, local_pos, STYLE.CHUNK_SIZE)
+			var visible_heat: float = _calculate_visible_heat(cell)
+
 			if visible_heat <= 0:
 				continue
-				
-			var color := HeatmapUtils.calculate_heat_color(
-				heatmap_data.config.start_color,
-				heatmap_data.config.end_color,
-				visible_heat,
-				STYLE.MAX_HEAT
+
+			var world_pos: Vector2 = NavigationUtils.GridUtils.cell_to_world(world_cell, STYLE.CELL_SIZE)
+			var rect: Rect2 = Rect2(
+				world_pos,
+				Vector2.ONE * STYLE.CELL_SIZE
 			)
-			
-			var world_pos := HeatmapUtils.grid_to_world(world_cell, STYLE.CELL_SIZE)
-			draw_rect(
-				Rect2(world_pos, Vector2.ONE * STYLE.CELL_SIZE),
-				color
-			)
-#endregion
+
+			var t: float = visible_heat / STYLE.MAX_HEAT
+			var color: Color = _get_cell_color(t, heatmap.config)
+			draw_rect(rect, color)
+
+func _calculate_visible_heat(cell: HeatCell) -> float:
+	var visible_heat: float = 0.0
+	for source_id in cell.sources:
+		var entity: Node2D = instance_from_id(source_id)
+		if not is_instance_valid(entity):
+			continue
+
+		if entity is Ant:
+			var colony: Node2D = entity.colony
+			if colony and (_debug_settings.get(source_id, false) or _debug_settings.get(colony.get_instance_id(), false)):
+				visible_heat += cell.sources[source_id]
+	return visible_heat
+
+func _get_cell_color(t: float, config: Pheromone) -> Color:
+	return config.start_color.lerp(config.end_color, t)
+
+#region Utility Functions
+func is_cell_navigable(pos: Vector2) -> bool:
+	if _nav_map == RID() or _nav_map == null or not NavigationServer2D.map_is_active(_nav_map):
+		return true
+
+	var regions: Array[RID] = NavigationServer2D.map_get_regions(_nav_map)
+	if regions.is_empty():
+		return true
+
+	for region in regions:
+		if region and NavigationServer2D.region_owns_point(region, pos):
+			return true
+
+	return false
+
+func get_cells_in_radius(world_pos: Vector2, radius: float, heat_type: String) -> Array[Dictionary]:
+	var center_cell: Vector2i = NavigationUtils.GridUtils.world_to_cell(world_pos, STYLE.CELL_SIZE)
+	var cells_radius: int = ceili(radius / STYLE.CELL_SIZE)
+	var found_cells: Array[Dictionary] = []
+
+	var chunk_radius: int = ceili(float(cells_radius) / STYLE.CHUNK_SIZE)
+	var center_chunk: Vector2i = NavigationUtils.GridUtils.world_to_chunk(center_cell, STYLE.CHUNK_SIZE)
+
+	var heatmap: HeatmapInstance = _heatmaps[heat_type]
+	for dx in range(-chunk_radius, chunk_radius + 1):
+		for dy in range(-chunk_radius, chunk_radius + 1):
+			var check_chunk: Vector2i = center_chunk + Vector2i(dx, dy)
+			if not heatmap.chunks.has(check_chunk):
+				continue
+
+			var chunk: HeatChunk = heatmap.chunks[check_chunk]
+			for local_pos in chunk.cells:
+				var cell: HeatCell = chunk.cells[local_pos]
+				var world_cell: Vector2i = NavigationUtils.GridUtils.chunk_to_world_cell(check_chunk, local_pos, STYLE.CHUNK_SIZE)
+				var cell_pos: Vector2 = NavigationUtils.GridUtils.cell_to_world(world_cell, STYLE.CELL_SIZE)
+
+				if cell.heat > 0 and world_pos.distance_to(cell_pos) <= radius:
+					found_cells.append({
+						"position": cell_pos,
+						"chunk_pos": check_chunk,
+						"local_pos": local_pos,
+						"heat": cell.heat,
+						"sources": cell.sources,
+						"cell": cell
+					})
+
+	return found_cells
